9396423cf9b1f786ff14340e051ee454671fc717
emilio <emilio.dorigatti@gmail.com>
2016-02-08 11:28:36 +0100

    data from the Biographical Dictionary of America

--- web_sources_corpus/biographical_dictionary_of_america.py
+++ web_sources_corpus/biographical_dictionary_of_america.py
@@ -2,26 +2,55 @@
 # -*- encoding: utf-8 -*-
 
 import re
-from requests import get
+import click
+import json
+import os
+import requests
 
-# The book has 10 volumes 
-VOLUMES = [n for n in xrange(1, 11)]
-# Must be filled
-URL_TEMPLATE = "https://archive.org/download/biographicaldict%02djohnuoft/biographicaldict%02djohnuoft_djvu.txt"
 
-items = []
+def get_volume(number, use_cache=True):
+    cached = '/tmp/bio_dir_of_america_{volume}.txt'.format(volume=number)
+    if use_cache and os.path.exists(cached):
+        with open(cached) as f:
+            content = f.read().decode('utf8')
+    else:
+        volume_url = 'https://archive.org/download/biographicaldict{volume:02d}johnuoft/' \
+                     'biographicaldict{volume:02d}johnuoft_djvu.txt'.format(volume=number)
+        r = requests.get(volume_url)
+        r.raise_for_status()
+        content = r.text
+        if use_cache:
+            with open(cached, 'w') as f:
+                f.write(content.encode('utf8'))
+    return content
 
-for volume in xrange(1, 11):
-    r = get(URL_TEMPLATE % (volume, volume))
-    volume_content = r.text
-    start_person = False
-    # Skip empty lines
-    for line in volume_content.split('\n'):
-        if not line:
-            continue
-        match = re.match(ur'([A-Z]+, [A-Z][a-z]+ ?),[^,]+,', line)
-        if match:
-            start_person = True
-            item = {'name': match.group(1)}
-            
-        items.append(item)
+
+@click.command()
+@click.argument('out-file', type=click.File('w'))
+@click.option('--cache/--no-cache', default=True)
+def main(out_file, cache):
+    items = []
+    current_item = None
+    for volume in xrange(1, 11):
+        print 'Volume', volume
+        for line in get_volume(volume, use_cache=cache).split('\n'):
+            if not line:
+                continue
+            match = re.match(ur'([A-Z]+, [A-Z][a-z]+ ?),[^,]+,', line)
+            if match:
+                if current_item:
+                    items.append(current_item)
+                print '%s' % match.group(1)
+                current_item = {
+                    'name': match.group(1),
+                    'bio': '',
+                }
+            elif current_item:
+                current_item['bio'] += line
+
+    print 'Got {} items'.format(len(items))
+    json.dump(items, out_file)
+
+
+if __name__ == '__main__':
+    main()
