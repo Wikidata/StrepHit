ad51729749bbf149ef2229bcd13a5ba35c893d18
emilio <emilio.dorigatti@gmail.com>
2016-02-09 09:43:30 +0100

    scraping the Dictionary of National Biography, 1901 supplement AND 1912 supplement

--- web_sources_corpus/web_sources_corpus/spiders/national_bio.py
+++ web_sources_corpus/web_sources_corpus/spiders/national_bio.py
@@ -3,14 +3,9 @@ from web_sources_corpus.spiders import BaseSpider
 from web_sources_corpus.items import WebSourcesCorpusItem
 
 
-class NationalBio1901Spider(BaseSpider):
-    name = "national_bio_1901"
+class NationalBioSpider(BaseSpider):
+    name = "national_bio"
     allowed_domains = ["en.wikisource.org"]
-    start_urls = (
-        'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_1901_supplement,_Volume_1',
-        'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_1901_supplement,_Volume_2',
-        'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_1901_supplement,_Volume_3',
-    )
 
     list_page_selectors = None
     detail_page_selectors = 'xpath:.//table[@class="prettytable"]//tr[4]//a/@href'
@@ -22,8 +17,14 @@ class NationalBio1901Spider(BaseSpider):
         'bio': 'clean:xpath:.//div[@id="mw-content-text"]//p//text()',
     }
 
-    def refine_item(self, response, item):
-        return super(NationalBio1901Spider, self).refine_item(response, item)
+    def __init__(self, year):
+        assert year in {'1901', '1912'}
+        self.start_urls = (
+            'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_%s_supplement,_Volume_1' % year,
+            'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_%s_supplement,_Volume_2' % year,
+            'https://en.wikisource.org/wiki/Dictionary_of_National_Biography,_%s_supplement,_Volume_3' % year
+        )
+        
 
     def get_name_from_title(self, response, title):
         return ''.join(title.split('(')[:-1]).strip()
