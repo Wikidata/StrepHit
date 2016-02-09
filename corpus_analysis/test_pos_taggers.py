f51ec0e81dd8235d6ac50a15e6d87b09a678d5f8
Marco Fossati <fossati@fbk.eu>
2016-02-09 10:16:21 +0100

    testing TreeTagger and NLTK with default params

---
+++ corpus_analysis/test_pos_taggers.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import click
+import json
+from utils import logger
+from sys import exit
+from time import time
+from nltk import pos_tag, word_tokenize
+from treetaggerwrapper import TreeTagger, make_tags
+
+
+LOGGER = logger.default_logger
+
+
+def tag(text, tt_home):
+    # Default NLTK's tokenizer
+    # TreebankWordTokenizer + PunktSentenceTokenizer
+    nltk_start = time()
+    tokens = word_tokenize(text)
+    # Default NLTK's POS tagger
+    # ?
+    # Use tagset='universal' for universal tagset
+    nltk_tagged = pos_tag(tokens)
+    nltk_end = time()
+    nltk_execution = nltk_end - nltk_start
+    LOGGER.info("NLTK took %f seconds" % nltk_execution)
+
+    # TreeTagger wrapper
+    # Tokenization: ?
+    # Default language: English
+    # English: trained on Penn treebank
+    # Default flags: -token -lemma -sgml -quiet -no-unknown
+    tt_start = time()
+    tt = TreeTagger(TAGDIR=tt_home)
+    raw_tags = tt.tag_text(text)
+    tt_end = time()
+    tt_execution = tt_end - tt_start
+    tt_tagged = make_tags(raw_tags)
+    LOGGER.info("TreeTagger took %f seconds" % tt_execution)
+    return (nltk_tagged, nltk_execution), (tt_tagged, tt_execution)
+
+
+@click.command()
+@click.argument('input-file', type=click.File('rb'))
+@click.option('--output-file', type=click.File('wb'), default='tagged.json')
+@click.option('--tt-home')
+def main(input_file, output_file, tt_home):
+    output = []
+    source = json.load(input_file)
+    LOGGER.info("Loaded input file '%s'" % input_file.name)
+    nltk_total_execution = tt_total_execution = float()
+    for item in source:
+        output_item = item
+        text = item.get('bio')
+        if not text:
+            LOGGER.info("No bio for '%s'. Skipping ..." % item.get('name'))
+            continue
+        else:
+            (nltk_tagged, nltk_execution), (tt_tagged, tt_execution) = tag(text, tt_home)
+            nltk_total_execution += nltk_execution
+            tt_total_execution += tt_execution
+            LOGGER.debug("NLTK output: %s" % nltk_tagged)
+            LOGGER.debug("TreeTagger output: %s" % tt_tagged)
+            output_item['nltk'] = nltk_tagged
+            output_item['TreeTagger'] = tt_tagged
+            output.append(output_item)
+            
+    json.dump(output, output_file, indent=2)
+    LOGGER.info("Tagged data dumped to '%s'" % output_file.name)
+    LOGGER.info("Total items = %d" % len(source))
+    LOGGER.info("NLTK total execution time = %f seconds" % nltk_total_execution)
+    LOGGER.info("TreeTagger total execution time = %f seconds" % tt_total_execution)
+    return 0
+
+    
+if __name__ == '__main__':
+    exit(main())
