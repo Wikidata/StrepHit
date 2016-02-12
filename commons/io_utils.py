b935fad1c00794c229d6f3a23e5c568f930e94c6
Marco Fossati <fossati@fbk.eu>
2016-02-12 19:17:07 +0100

    load the textual documents of scraped items, given a suitable item key

--- commons/io_utils.py
+++ commons/io_utils.py
@@ -6,11 +6,10 @@ import os
 from commons.logger import logger
 
 
-def load_corpus(corpus_dir):
-    """Load an input corpus from a directory.
-       Each input file must contain one JSON object per line.
-    """    
-    for path, subdirs, files in os.walk(corpus_dir):
+def load_scraped_items(items_dir):
+    """
+    """
+    for path, subdirs, files in os.walk(items_dir):
     	for name in files:
             f = os.path.join(path, name)
             with open(f) as source:
@@ -18,3 +17,16 @@ def load_corpus(corpus_dir):
                 for n, line in enumerate(source):
                     logger.debug("Processing item #%d ..." % n)
                     yield json.loads(line)
+
+
+def load_corpus(items_dir, document_key):
+    """Load an input corpus from a directory with scraped items, in a memory-efficient way.
+       Each input file must contain one JSON object per line.
+       :param str document_key: a scraped item dictionary key holding textual documents
+    """
+    for item in load_scraped_items(items_dir):
+        document = item.get(document_key)
+        if document:
+            yield document
+        else:
+            logger.warning("Skipped item with no textual document. Item: '%s'. Provided item key: '%s'" % (item, document_key))
