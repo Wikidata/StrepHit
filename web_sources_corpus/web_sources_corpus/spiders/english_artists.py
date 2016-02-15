28b8e5d14ab80864a02090ed7d7f5b7aa9eb156a
emilio <emilio.dorigatti@gmail.com>
2016-02-15 12:33:09 +0100

    spider for the Dictionary of Artists of the English School

---
+++ web_sources_corpus/web_sources_corpus/spiders/english_artists.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import json
+from scrapy import Spider, Request
+from web_sources_corpus.items import WebSourcesCorpusItem
+from web_sources_corpus import utils
+
+
+class EnglishArtistsSpider(Spider):
+    name = "english_artists"
+    allowed_domains = ["en.wikisource.org"]
+    start_urls = (
+        'https://en.wikisource.org/wiki/A_Dictionary_of_Artists_of_the_English_School',
+    )
+
+    def parse(self, response):
+        for url in response.xpath(
+                    './/table[@class="headertemplate"]//p[2]/a[not(@class="new")]/@href'
+                ).extract():
+            yield Request('https://en.wikisource.org' + url, self.parse_detail)
+
+    def parse_detail(self, response):
+        item = None
+        for each in response.xpath(
+                    './/div[@class="tiInherit"]/parent::div/*'
+                )[3:]:
+            content = each.xpath('child::node()')
+            if content and content[0].xpath('local-name()').extract() == ['span']:
+                if item:
+                    yield self.finalize(item)
+
+                item = WebSourcesCorpusItem(
+                    url=response.url,
+                    name=' '.join(self.text_from_node(c) for c in content[:3]),
+                    bio=utils.clean_extract(each, './/text()', sep=' '),
+                )
+
+                if each.xpath('./i'):
+                    item['other'] = {
+                        'profession': utils.clean_extract(each, './i//text()')
+                    }
+                
+                assert item['name'] and len(item['name']) > 3
+            elif item:
+                item['bio'] += '\n' + utils.clean_extract(each, './/text()', sep=' ')
+
+        if item:
+            yield self.finalize(item)
+
+    def finalize(self, item):
+        if 'other' in item:
+            item['other'] = json.dumps(item['other'])
+        item['bio'] = utils.clean(item['bio'])
+        item['name'] = utils.clean(','.join(item['name'].split(',')[:-1]))
+        return item
+
+    def text_from_node(self, node):
+        return (utils.clean_extract(node, './/text()', sep=' ')
+                if node.xpath('local-name()').extract()
+                else utils.clean(node.extract()))
