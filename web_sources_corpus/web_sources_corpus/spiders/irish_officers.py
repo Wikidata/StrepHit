204ee1738a54c7f0aa65025ee0dd298b680fe20d
emilio <emilio.dorigatti@gmail.com>
2016-02-15 11:29:58 +0100

    spider for the Chronicle of the law officers of Ireland

---
+++ web_sources_corpus/web_sources_corpus/spiders/irish_officers.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from scrapy import Request
+from scrapy import Spider
+from web_sources_corpus import utils
+from web_sources_corpus.items import WebSourcesCorpusItem
+
+
+class IrishOfficersSpider(Spider):
+    name = "irish_officers"
+    allowed_domains = ["en.wikisource.org"]
+    start_urls = (
+        'https://en.wikisource.org/wiki/Chronicle_of_the_law_officers_of_Ireland',
+    )
+
+    def parse(self, response):
+        for url in response.xpath(
+                    './/a[starts-with(@title, "Chronicle of the law officers of Ireland/")]/@href'
+                ).extract()[4:-1]:
+            yield Request('https://en.wikisource.org' + url, self.parse_detail)
+            
+    def parse_detail(self, response):
+        for each in response.xpath(
+                    './/div[@id="headerContainer"]/following-sibling::div//p'
+                ):
+            yield WebSourcesCorpusItem(
+                url=response.url,
+                name=utils.clean_extract(each, './span//text()'),
+                bio=utils.clean_extract(each, './/text()', sep=' '),
+            )
+
+    def refine_item(self, response, item):
+        return super(IrishOfficersSpider, self).refine_item(response, item)
