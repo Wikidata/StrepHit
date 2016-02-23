2d058b087c3c20ded2e907d53fe8a979bf6ec1c7
Marco Fossati <fossati@fbk.eu>
2016-02-23 20:10:29 +0100

    comply to latest project structure

--- strephit/corpus_analysis/__init__.py
+++ strephit/corpus_analysis/__init__.py
@@ -1,15 +1,3 @@
-import click
-from strephit.corpus_analysis import extract_framenet_frames, rank_verbs
-
-
-CLI_COMMANDS = {
-    'extract_framenet_frames': extract_framenet_frames.main,
-    'rank_verbs': rank_verbs.main
-}
-
-
-@click.group(name='corpus_analysis', commands=CLI_COMMANDS)
-@click.pass_context
-def cli(ctx):
-    """ Corpus analysis module """
-    pass
+import cli
+import extract_framenet_frames
+import rank_verbs
