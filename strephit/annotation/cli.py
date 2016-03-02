8db8ff1ba299968808cbf34ac4fefc9f22479d58
Marco Fossati <fossati@fbk.eu>
2016-03-02 18:11:55 +0100

    CrowdFlower data units CSV creation

--- strephit/annotation/cli.py
+++ strephit/annotation/cli.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 import click
-from strephit.annotation import post_job, pull_results
+from strephit.annotation import create_crowdflower_input, post_job, pull_results
 
 
 CLI_COMMANDS = {
+    'create_crowdflower_input': create_crowdflower_input.main,
     'post_job': post_job.main,
     'pull_results': pull_results.main,
 }
