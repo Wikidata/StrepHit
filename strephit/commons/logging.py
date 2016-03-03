f425bf9eb2872e357c3423cc5c133d2b48496fe2
Marco Fossati <fossati@fbk.eu>
2016-03-03 18:28:58 +0100

    refactored debug message for sent HTTP request data

--- strephit/commons/logging.py
+++ strephit/commons/logging.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
 from __future__ import absolute_import
 import tempfile
 import sys
@@ -7,7 +8,7 @@ import logging
 import logging.config
 import yaml
 import os
-
+from urllib import unquote_plus
 
 LEVELS = {
     'DEBUG': logging.DEBUG,
@@ -46,7 +47,7 @@ def setup():
             'debug_file_handler': {
                 'formatter': 'strephit',
                 'level': 'DEBUG', 
-                'filename': 'strephit-debug.log', 
+                'filename': 'strephit_debug.log', 
                 'mode': 'w',
                 'class': 'logging.FileHandler', 
                 'encoding': 'utf8'
@@ -60,3 +61,18 @@ def setLogLevel(module, level):
     if level in LEVELS:
         module = '' if module == 'root' else module
         logging.getLogger(module).setLevel(level)
+
+
+def log_request_data(http_response, logger):
+    """
+    Send a debug log message with basic information of the HTTP request that was sent for the given HTTP response.
+    :param requests.models.Response http_response: HTTP response object
+    """
+    sent_request = {
+        'method': http_response.request.method,
+        'url': http_response.request.url,
+        'headers': http_response.request.headers,
+        'decoded_body': unquote_plus(repr(http_response.request.body))
+    }
+    logger.debug("Request sent: %s" % sent_request)
+    return 0
