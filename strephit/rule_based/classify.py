4f993e85b2c19ed6c51e0a930671a5a1b0255a26
Marco Fossati <fossati@fbk.eu>
2016-04-22 13:56:28 +0200

    first step towards rule-based classification for companies

---
+++ strephit/rule_based/classify.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import logging
+from sys import exit
+
+import os
+
+
+import click
+import codecs
+import json
+import random
+from collections import defaultdict
+from urllib import quote
+from rfc3987 import parse  # URI/IRI validation
+from date_normalizer import DateNormalizer
+from resources.frame_repo import LU_FRAME_MAP
+from strephit.commons.date_normalizer import DateNormalizer
+from strephit.commons.scoring import compute_score, AVAILABLE_SCORES
+from strephit.commons.stopwords import StopWords
+
+
+logger = logging.getLogger(__name__)
+
+NORMALIZER = DateNormalizer()
+
+
+def label_sentence(entity_linking_results, debug, numerical):
+    """Produce a labeled sentence by comparing the linked entities to the frame definition
+
+    :param str entity_linking_results: path to JSON file containing the results of the
+                                       entity linking
+    :param bool debug: Print debugging information
+    :param bool numerical: Normalize numerical FEs
+    :return: labeled data for each sentence
+    :rtype: dict
+    """
+    labeled = {}
+    links = json.load(codecs.open(entity_linking_results, 'rb', 'utf-8'))
+    sentence, val = links.items()[0]
+    labeled['sentence'] = sentence
+    labeled['FEs'] = defaultdict(list)
+    # Tokenize by splitting on spaces
+    sentence_tokens = sentence.split()
+    if debug:
+        print 'SENTENCE: %s' % sentence
+        print 'TOKENS: %s' % sentence_tokens
+    frames = []
+    for lu in LU_FRAME_MAP:
+        lu_tokens = lu['lu']['tokens']
+        # Check if a sentence token matches a LU token and assign frames accordingly
+        for sentence_token in sentence_tokens:
+            if sentence_token in lu_tokens:
+                if debug:
+                    print 'TOKEN "%s" MATCHED IN LU TOKENS' % sentence_token
+                labeled['lu'] = lu['lu']['lemma']
+                frames = lu['lu']['frames']
+                if debug:
+                    print 'LU LEMMA: %s' % labeled['lu']
+                    print 'FRAMES: %s' % [frame['frame'] for frame in frames]
+                # Frame processing
+                for frame in frames:
+                    FEs = frame['FEs']
+                    types_to_FEs = frame['DBpedia']
+                    if debug:
+                        print 'CURRENT FRAME: %s' % frame['frame']
+                        print 'FEs: %s' % FEs
+                    core = False
+                    assigned_fes = []
+                    for diz in val:
+                        # Filter out linked stopwords
+                        if diz['chunk'].lower() in StopWords.words('italian'):
+                            continue
+
+                        chunk = {
+                            'chunk': diz['chunk'],
+                            'uri': diz['uri'],
+                            'score': diz['score']
+                        }
+
+                        types = diz['types']
+                        #### FE assignment ###
+                        for t in types:
+                            for mapping in types_to_FEs:
+                                # Strip DBpedia ontology namespace
+                                looked_up = mapping.get(t[28:])
+                                if looked_up:
+                                    if debug:
+                                        print 'Chunk "%s" has an ontology type "%s" that maps to FE "%s"' % (chunk['chunk'], t[28:], looked_up)
+                                    ### Frame disambiguation strategy, part 1 ###
+                                    # LAPSE ASSIGNMENT
+                                    # If there is AT LEAST ONE core FE, then assign that frame
+                                    # TODO strict assignment: ALL core FEs must be found
+                                    # Will not work if the FEs across competing frames have the same ontology type
+                                    # e.g., Attività > Squadra and Partita > [Squadra_1, Squadra_2]
+
+                                    # Check if looked up FE is core
+                                    for fe in FEs:
+                                        if type(looked_up) == list:
+                                            for shared_type_fe in looked_up:
+                                                shared_fe_type = fe.get(shared_type_fe)
+                                                # TODO overwritten value
+                                                if shared_fe_type:
+                                                    chunk['type'] = shared_fe_type
+                                                if shared_fe_type == 'core':
+                                                    if debug:
+                                                        print 'Mapped FE "%s" is core for frame "%s"' % (shared_type_fe, frame['frame'])
+                                                    core = True
+                                        else:
+                                            fe_type = fe.get(looked_up)
+                                            if fe_type:
+                                                chunk['type'] = fe_type
+                                            if fe_type == 'core':
+                                                if debug:
+                                                    print 'Mapped FE "%s" is core for frame "%s"' % (looked_up, frame['frame'])
+                                                core = True
+                                    ### FE disambiguation strategy ###
+                                    # If multiple FEs have the same ontology type, e.g., [Vincitore, Perdente] -> Club
+                                    # BASELINE = random assignment
+                                    # Needs to be adjusted by humans
+                                    if type(looked_up) == list:
+                                        chosen = random.choice(looked_up)
+                                        chunk['FE'] = chosen
+                                        # Avoid duplicates
+                                        if chunk not in assigned_fes:
+                                            assigned_fes.append(chunk)
+                                    else:
+                                        chunk['FE'] = looked_up
+                                        # Avoid duplicates
+                                        if chunk not in assigned_fes:
+                                            assigned_fes.append(chunk)
+                    # Continue to next frame if NO core FE was found
+                    if not core:
+                        if debug:
+                            print 'No core FE for frame "%s": skipping' % frame['frame']
+                        continue
+                    # Otherwise assign frame and previously stored FEs
+                    else:
+                        if debug:
+                            print 'ASSIGNING FRAME: %s' % frame['frame']
+                            print 'ASSIGNING FEs: %s' % assigned_fes
+                        ### Frame disambiguation strategy, part 2 ###
+                        # If at least 1 core FE is detected in multiple frames:
+                        # BASELINE = random assignment
+                        # Needs to be adjusted by humans
+                        current_frame = frame['frame']
+                        previous_frame = labeled.get('frame')
+                        if previous_frame:
+                            previous_FEs = labeled['FEs']
+                            choice = random.choice([previous_frame, current_frame])
+                            if debug:
+                                print 'CORE FES FOR MULTIPLE FRAMES WERE DETECTED. MAKING A RANDOM ASSIGNMENT: %s' % choice
+                            if choice == current_frame:
+                                labeled['frame'] = current_frame
+                                labeled['FEs'] = assigned_fes
+                        else:
+                            labeled['frame'] = current_frame
+                            labeled['FEs'] = assigned_fes
+
+    # Normalize + annotate numerical FEs (only if we could disambiguate the sentence)
+    if labeled.get('frame') and numerical:
+        if debug:
+            print 'LABELING AND NORMALIZING NUMERICAL FEs ...'
+        for (start, end), tag, norm in NORMALIZER.normalize_many(sentence):
+            chunk = sentence[start:end]
+            if debug:
+                print 'Chunk [%s] normalized into [%s], tagged as [%s]' % (chunk, norm, tag)
+            fe = {  # All numerical FEs are extra ones and their values are literals
+                'chunk': chunk,
+                'FE': tag,
+                'type': 'extra',
+                'literal': norm,
+                'score': 1.0
+            }
+            labeled['FEs'].append(fe)
+    return labeled
+
+
+def process_dir(indir, debug, numerical):
+    """Walk into the input directory and process all the entity linking results,
+    creating the labeled data
+
+    :param str indir: Path to the directory with the entity linking results
+    :param bool debug: Print debugging information
+    :param bool numerical: Normalize numerical FEs
+    :return: The labeled data
+    :rtype: dict
+    """
+    processed = []
+    for path, subdirs, files in os.walk(indir):
+        for name in files:
+            f = os.path.join(path, name)
+            labeled = label_sentence(f, debug, numerical)
+            # Filename is {WIKI_ID}.{SENTENCE_ID}(.{extension})?
+            labeled['id'] = '.'.join(name.split('.')[:2])
+
+            processed.append(labeled)
+            if debug:
+                print 'LABELED: %s' % labeled
+    return processed
+
+
+@click.command()
+@click.argument('linked-dir', type=click.Path(exists=True, file_okay=False))
+@click.argument('labeled_out', default='labeled.json')
+@click.option('--score', type=click.Choice(['arithmetic-mean', 'weighted-mean',
+                                            'f-score', '']))
+@click.option('--core-weight', default=2)
+@click.option('--score-fes/--no-score-fes', help='Score individual FEs')
+@click.option('--debug/--no-debug', default=False)
+@click.option('--numerical/--no-numerical', default=True)
+def main(linked_dir, labeled_out, score, core_weight, score_fes, debug, numerical):
+    """
+    Rule-based classifier
+    """
+    labeled = process_dir(linked_dir, score_fes, debug, numerical)
+
+    if score:
+        for sentence in labeled:
+            sentence['score'] = compute_score(sentence, score, core_weight)
+            if not score_fes:
+                [fe.pop('score') for fe in sentence['FEs']]
+
+    with codecs.open(labeled_out, 'w', 'utf8') as f:
+        json.dump(labeled, f, ensure_ascii=False, indent=2)
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
---
+++ strephit/rule_based/resources/frame_repo.py
@@ -0,0 +1,419 @@
+# -*- encoding: utf-8 -*-
+FRAME_REPO = [
+    {
+        "lu":
+        {
+            "lemma": "esordire",
+            "tokens":
+            [
+                "esordendo",
+                "esordendovi",
+                u"esordì",
+                u"esordirà",
+                "esordiranno",
+                "esordire",
+                "esordirono",
+                "esordirvi",
+                "esordisca",
+                "esordisce",
+                "esordiscono",
+                "esordito",
+                "esordiva"
+            ],
+            "frames":
+            [
+                {
+                    "frame": u"Attività",
+                    "FEs":
+                    [
+                        { "Competizione": "core" },
+                        { "Squadra": "core" },
+                        { "Agente": "extra" },
+                        { "Durata": "extra" },
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": "Squadra" },
+                        { "Place": "Luogo" },
+                        { "SoccerPlayer": "Agente" }
+                    ]
+                }
+            ]
+        }
+    },    
+    {
+        "lu": 
+        {
+            "lemma": "giocare",
+            "tokens":
+            [
+                "gioca",
+                "giocai",
+                "giocammo",
+                "giocandoci",
+                "giocando",
+                "giocandogli",
+                "giocandola",
+                "giocandole",
+                "giocandoli",
+                "giocandolo",
+                "giocandone",
+                "giocandosela",
+                "giocandoselo",
+                "giocandosi",
+                "giocandovi",
+                "giocane",
+                "giocano",
+                "giocante",
+                "giocanti",
+                "giocarci",
+                "giocare",
+                "giocar",
+                "giocargli",
+                "giocarla",
+                "giocarle",
+                "giocarli",
+                "giocarlo",
+                "giocarne",
+                "giocarono",
+                "giocarsela",
+                "giocarselo",
+                "giocarsi",
+                "giocarvi",
+                "giocasi",
+                "giocasse",
+                "giocassero",
+                "giocata",
+                "giocate",
+                "giocatesi",
+                "giocatevi",
+                "giocati",
+                "giocato",
+                "giocava",
+                "giocavamo",
+                "giocavano",
+                "giocavi",
+                "giocavo",
+                u"giocherà",
+                "giocheranno",
+                "giocherebbe",
+                "giocherebbero",
+                "giocherei",
+                "giocheremo",
+                "giocheresti",
+                "giocherete",
+                u"giocherò",
+                "giochiamo",
+                "giochi",
+                "giochino",
+                "gioco",
+                u"giocò"
+            ],
+            "frames":
+            [
+                {
+                    "frame": "Partita",
+                    "FEs":
+                    [
+                        { "Squadra_1": "core" },
+                        { "Squadra_2": "core" },
+                        { "Competizione": "extra"},
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" },
+                        { "Punteggio": "extra" },
+                        { "Classifica": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": ["Squadra_1", "Squadra_2"] },
+                        { "Place": "Luogo" }
+                    ]
+                },
+                {
+                    "frame": u"Attività",
+                    "FEs":
+                    [
+                        { "Competizione": "core" },
+                        { "Squadra": "core" },
+                        { "Agente": "extra" },
+                        { "Durata": "extra" },
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": "Squadra" },
+                        { "Place": "Luogo" },
+                        { "SoccerPlayer": "Agente" }
+                    ]
+                }
+            ]
+        }
+    },
+    {
+        "lu":
+        {
+            "lemma": "perdere",
+            "tokens":
+            [
+                "perda",
+                "perde",
+                "perdemmo",
+                "perdendo",
+                "perdendola",
+                "perdendole",
+                "perdendoli",
+                "perdendolo",
+                "perdendone",
+                u"perderà",
+                "perderanno",
+                "perdere",
+                "perderla",
+                "perderlo",
+                "perderne",
+                "perdersi",
+                "perdesse",
+                "perdessero",
+                "perdessimo",
+                "perdete",
+                "perdeva",
+                "perdevano",
+                "perdilo",
+                "perdono",
+                "persa",
+                "perse",
+                "persero",
+                "persi",
+                "perso"
+            ],
+            "frames":
+            [
+                {
+                    "frame": "Sconfitta",
+                    "FEs":
+                    [
+                        { "Vincitore": "core" },
+                        { "Perdente": "core" },
+                        { "Competizione": "extra"},
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" },
+                        { "Punteggio": "extra" },
+                        { "Classifica": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": ["Vincitore", "Perdente"] },
+                        { "Place": "Luogo" }
+                    ]
+                }
+            ]
+        }
+    },
+    {
+        "lu":
+        {
+            "lemma": "rimanere",
+            "tokens":
+            [
+                "rimane",
+                "rimanendo",
+                "rimanendoci",
+                "rimanendolo",
+                "rimanendone",
+                "rimanendovi",
+                "rimaner",
+                "rimanerci",
+                "rimanere",
+                "rimanermi",
+                "rimanervi",
+                "rimanesse",
+                "rimanessero",
+                "rimaneva",
+                "rimanevano",
+                "rimanga",
+                "rimangono",
+                "rimani",
+                u"rimarrà",
+                "rimarrai",
+                "rimarranno",
+                "rimarrebbe",
+                "rimase",
+                "rimasero",
+                "rimasi",
+                "rimasta",
+                "rimaste",
+                "rimasti",
+                "rimasto"
+            ],
+            "frames":
+            [
+                {
+                    "frame": u"Attività",
+                    "FEs":
+                    [
+                        { "Competizione": "core" },
+                        { "Squadra": "core" },
+                        { "Agente": "extra" },
+                        { "Durata": "extra" },
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": "Squadra" },
+                        { "Place": "Luogo" },
+                        { "SoccerPlayer": "Agente" }
+                    ]
+                },
+                {
+                    "frame": "Stato",
+                    "FEs":
+                    [
+                        { u"Entità": "core" },
+                        { "Stato": "core" },
+                        { "Durata": "extra" },
+                        { "Luogo": "extra" },
+                        { "Squadra": "extra" },
+                        { "Tempo": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerClub": "Squadra" },
+                        { "SoccerPlayer": u"Entità" },
+                        { "Place": "Luogo" }
+                    ]
+                }
+            ]
+        }
+    },
+    {
+        "lu":
+        {
+            "lemma": "vincere",
+            "tokens":
+            [
+                "vincano", 
+                "vinca", 
+                "vincemmo", 
+                "vincendoci", 
+                "vincendogli", 
+                "vincendola", 
+                "vincendole", 
+                "vincendoli", 
+                "vincendolo", 
+                "vincendone", 
+                "vincendosi", 
+                "vincendo", 
+                "vincendovi", 
+                "vincente", 
+                "vincenti", 
+                "vincerai", 
+                "vinceranno", 
+                u"vincerà", 
+                "vincerci", 
+                "vincerebbero", 
+                "vincerebbe", 
+                "vincerei", 
+                "vinceremo", 
+                "vincerete", 
+                "vincere", 
+                "vincergli", 
+                "vincerla", 
+                "vincerle", 
+                "vincerli", 
+                "vincerlo", 
+                "vincermi", 
+                "vincerne", 
+                u"vincerò", 
+                "vincersi", 
+                "vincer", 
+                "vincervi", 
+                "vincessero", 
+                "vincesse", 
+                "vincessi", 
+                "vincesti", 
+                "vincete", 
+                "vincevano", 
+                "vinceva", 
+                "vince", 
+                "vincevo", 
+                "vinciamo", 
+                "vinciate", 
+                "vinci", 
+                "vincono", 
+                "vinco", 
+                "vinsero", 
+                "vinse", 
+                "vinsi", 
+                "vinta", 
+                "vinte", 
+                "vinti", 
+                "vinto"
+            ],
+            "frames":
+            [
+                {
+                    "frame": "Vittoria",
+                    "FEs":
+                    [
+                        { "Vincitore": "core" },
+                        { "Perdente": "core" },
+                        { "Competizione": "extra"},
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" },
+                        { "Punteggio": "extra" },
+                        { "Classifica": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerClub": ["Vincitore", "Perdente"] },
+                        { "Place": "Luogo" }
+                    ]
+                },
+                {
+                    "frame": "Trofeo",
+                    "FEs":
+                    [
+                        { "Concorrente": "core" },
+                        { "Competizione": "core" },
+                        { "Squadra": "extra" },
+                        { "Premio": "extra" },
+                        { "Luogo": "extra" },
+                        { "Tempo": "extra" },
+                        { "Punteggio": "extra" },
+                        { "Classifica": "extra" }
+                    ],
+                    "DBpedia":
+                    [
+                        { "SoccerLeague": "Competizione" },
+                        { "SoccerTournament": "Competizione" },
+                        { "SoccerLeagueSeason": "Competizione" },
+                        { "SoccerPlayer": "Concorrente" },
+                        { "SoccerClub": "Squadra" },
+                        { "Place": "Luogo" }
+                    ]
+                }
+            ]
+        }
+    }
+]
