Wikidata/StrepHit

commit 0261af83971cadf2ea2b33f577c2b03afa1a8f90

prepend 'answer_' to radio button names in CML template

strephit/annotation/generate_cml.py
@@ -26,7 +26,7 @@ TOKEN_TEMPLATE = '''
     <!-- BEGIN token %(question_num)d -->
     {%% if %(token_field)s != 'No data available' %%}
     <div class="span2">
-        <cml:radios label="{{%(token_field)s}}" class="rando" name="{{%(token_field)s}}" validates="required" gold="true">
+        <cml:radios label="{{%(token_field)s}}" class="rando" name="name="answer_{{%(token_field)s}}"{{%(token_field)s}}" validates="required" gold="true">
 
             <cml:radio label="None"></cml:radio>
             %(fe_blocks)s

commit f4037a3e06744083de7be9997055944025999f7a

fix: prepend 'answer_' to radio button names (broken in 0261af83971cadf2ea2b33f577c2b03afa1a8f90)

strephit/annotation/generate_cml.py
@@ -26,7 +26,7 @@ TOKEN_TEMPLATE = '''
     <!-- BEGIN token %(question_num)d -->
     {%% if %(token_field)s != 'No data available' %%}
     <div class="span2">
-        <cml:radios label="{{%(token_field)s}}" class="rando" name="name="answer_{{%(token_field)s}}"{{%(token_field)s}}" validates="required" gold="true">
+        <cml:radios label="{{%(token_field)s}}" class="rando" name="answer_{{%(token_field)s}}" validates="required" gold="true">
 
             <cml:radio label="None"></cml:radio>
             %(fe_blocks)s